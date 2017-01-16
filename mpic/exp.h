a7adead6a1710db43e67363b22fc87ecea084a6e
weizili <zieckey@gmail.com>
2017-01-16 13:21:24 +0800

    Add set (BUILD_SHARED_LIBS 1) to gflags CMakeList.txt

--- mpic/exp.h
+++ mpic/exp.h
@@ -6,8 +6,8 @@
 
 #ifdef H_OS_WINDOWS
 H_LINK_LIB("libmpic")
-H_LINK_LIB("libglog_static")
+H_LINK_LIB("libglog")
 H_LINK_LIB("Ws2_32")
-H_LINK_LIB("gflags_static")
+H_LINK_LIB("gflags")
 H_LINK_LIB("ShLwApi")
 #endif
