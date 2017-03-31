492a33e4a9d63e740a2c0fa16650c290fb22c442
weizili <zieckey@gmail.com>
2017-03-31 22:04:43 +0800

    Add CMakeLists.txt

--- examples/daemon/main.cc
+++ examples/daemon/main.cc
@@ -7,16 +7,20 @@
 #include "daemonizer.h"
 
 int main(int argc, char* argv[]) {
-    printf("pid=%d starting ...\n", getpid());
     pid_t start_pid = getpid();
+    printf("pid=%d starting ...\n", start_pid);
     bool is_daemon = false;
     if (argc == 2 && strcmp(argv[1], "-d") == 0) {
         is_daemon = true;
         mpic::Daemonizer daemon;
         daemon.Init();
+    } else if (argc == 2 && strcmp(argv[1], "-h") == 0) {
+        printf("Usage : %s -d\n", argv[0]);
+        return 0;
     }
 
     pid_t pid = getpid();
+    printf("pid=%d", pid);
     if (is_daemon) {
         assert(pid != start_pid);
     } else {
